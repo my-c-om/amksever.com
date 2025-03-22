21c1df6003ebd8d5d4793f36dcb6b02b8cdba721
my-c-om <athuyrain@gmail.com>
2025-03-22 16:23:32 +0630

    Update push.sh
    Add script update

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git init
 git add index.html
 git commit -m "First commit - AM Kyaw Social"
 git branch -M main
-git remote add origin https://github.com/YOUR_GITHUB_USERNAME/amkyaw-social.git
+git remote add origin https://github.com/my-c-om/amkyaw-social.git
 git push -u origin main
