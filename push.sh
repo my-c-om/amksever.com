fbd2fd5cf175a9464e120b79020455ae3b6da984
amkweb <athuyrain@gmail.com>
2025-04-06 11:19:30 +0630

    Delete push.sh
    Signed-off-by: amkweb <athuyrain@gmail.com>

--- push.sh
+++
@@ -1,6 +0,0 @@
-git init
-git add index.html
-git commit -m "First commit - AM Kyaw Social"
-git branch -M main
-git remote add origin https://github.com/my-c-om/amkyaw-social.git
-git push -u origin main
